2710d5b44cf9f7b2a9c447fafb09df9eb7f030f5
Oliver Skanberg-Tippen <oskanberg@andigital.com>
2016-03-01 13:49:39 +0000

    add handling for individual lines

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type Status struct {
@@ -17,6 +20,9 @@ type Report struct {
 }
 
 var statuses []Report
+var lastStatusCheck time.Time
+
+const MIN_STATUS_POLL_PERIOD = 2
 
 func updateStatusInformation() error {
 	url := "https://api.tfl.gov.uk/line/mode/tube/status"
@@ -37,18 +43,55 @@ func updateStatusInformation() error {
 		return err
 	}
 
+	statuses = data
 	return nil
 }
 
-func main() {
-	err := updateStatusInformation()
-	if err != nil {
+func getStatusString(line Report) string {
+	// simplest case, this is just a single 'segment'
+	if len(line.LineStatuses) == 1 {
+		return line.Name + " :: " + line.LineStatuses[0].StatusSeverityDescription + "\n"
+	}
+
+	//TODO: what if there is more than one segment?
+	return ""
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if time.Since(lastStatusCheck).Minutes() > MIN_STATUS_POLL_PERIOD {
+		err := updateStatusInformation()
+		if err != nil {
+			io.WriteString(w, "There was an error getting information from TFL")
+		}
+	}
+
+	queryLine := r.URL.Query().Get("line")
+	if len(queryLine) == 0 {
+		w.WriteHeader(200)
+		for _, line := range statuses {
+			io.WriteString(w, getStatusString(line))
+		}
 		return
 	}
 
 	for _, line := range statuses {
-		if len(line.LineStatuses) == 1 {
-			fmt.Println(line.Name, line.LineStatuses)
+		if strings.ToLower(line.Name) == strings.ToLower(queryLine) {
+			io.WriteString(w, getStatusString(line))
+			return
 		}
 	}
+
+	io.WriteString(w, "Not a recognised line")
+}
+
+func main() {
+	var err error
+	lastStatusCheck, err = time.Parse(time.RFC3339, "1970-01-01T00:00:00+00:00")
+	if err != nil {
+		return
+	}
+
+	http.HandleFunc("/", handleRequest)
+	fmt.Println("Serving on port 1123")
+	http.ListenAndServe(":1123", nil)
 }
